9d0e5615165a4702b54fa2ceb4b700b2ef0cefdd
FProject18 <139376988+FaisalBAkbar544@users.noreply.github.com>
2024-06-25 01:14:49 +0800

    Bismillah BackEndDeveloper 3 digits

---
+++ src/t4kecakapanbergithub/T4KecakapanBergithub.java
@@ -0,0 +1,13 @@
+package t4kecakapanbergithub;
+
+/**
+ *
+ * @author Muhammad Faisal Akbar
+ */
+public class T4KecakapanBergithub {
+
+    public static void main(String[] args) {
+        System.out.println("Muhammad Faisal Akbar");
+    }
+
+}
